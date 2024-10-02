0cb2eb211d57c7fda0ab7dc44b16ce7b2790d2e1
Patryk Olszewski <patrykolszewski420@gmail.com>
2024-10-02 18:21:10 +0200

    akordeon

--- src/js/script.js
+++ src/js/script.js
@@ -104,24 +104,20 @@ const select = {
       const thisProduct = this;
   
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable); //do wyjaśnienia
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
   
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
-        // preventDefault();
-        /* ReferenceError: preventDefault is not defined
-        at HTMLElement.<anonymous> (script.js:112:9) */
+        event.preventDefault();
+        
         /* find active product (product that has active class) */
-        const activeProduct = document.querySelectorAll(select.all.menuProductsActive);
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
      
         /* if there is active product and it's not thisProduct.element, remove class active from it */
           if (activeProduct && activeProduct !== thisProduct.element){
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
-      /* TypeError: Cannot read properties of undefined (reading 'remove')
-        at HTMLElement.<anonymous> (script.js:119:35)
-        (anonymous) @ script.js:119 */
 
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
